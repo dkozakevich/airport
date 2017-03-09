2118f204965c9108f2f0f52d279cf3bef7ab24b3
dkozakevich <kozakevich.dmitry@gmail.com>
2017-03-09 23:21:19 +0200

    ticekts.java has been changed

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,6 +1,15 @@
 
 public class Ticket {
+    private double price;
+    private boolean isAdult;
+    private boolean isBaggageOverweighted;
+    private String flight;
+    private String flightClass;
+    private String status;
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
